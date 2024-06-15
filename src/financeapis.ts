3117dd70ce37cead8c023c40e3eac477e3f52bf8
frmuller41182 <frmuller@amazon.com>
2024-06-15 15:49:48 +0200

    Progress on 6-15-2024 1. Started to make progress on Backend configuration 2. Successfully finished the response.ts file to abstract response configuration for my endpoints, now working on the financeapi router file to add the different apis I want for my project.

--- src/financeapis.ts
+++ src/financeapis.ts
@@ -5,3 +5,19 @@ import { z } from "zod";
 //import {catchErrors} from "./errors";
 
 const router = Router();
+
+router.get("/", async (req, res) => {
+  const forums = await db.user.findMany();
+  send(res).ok(forums);
+});
+
+router.get("/:id", async (req, res) => {
+  const forum = await db.user.findUnique({
+    where: { userId: Number(req.params.id) },
+  });
+  if (!forum) {
+    send(res).notFound();
+    return;
+  }
+  send(res).ok(forum);
+});
